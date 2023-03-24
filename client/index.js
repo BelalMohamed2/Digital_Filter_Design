80b54e947699018dde42ade1b3e569e6fac17d8e
Belal Abdul-kreem <94145068+BelalMohamed2@users.noreply.github.com>
2023-03-24 18:06:05 +0200

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,232 @@
+const filterDesignMagnitude = document.querySelector('#filter-mag-response')
+const filterDesignPhase = document.querySelector('#filter-phase-response')
+const allPassPhase = document.getElementById('all-pass-phase-response');
+const finalPhase = document.getElementById('final-filter-phase-response');
+const checkList = document.getElementById('list1');
+const zero_mode_btn = document.getElementById("zero")
+const pole_mode_btn = document.getElementById("pole")
+const all_pass_clear_btn =  document.getElementById("clear_allpass")
+const modes_btns = [zero_mode_btn, pole_mode_btn]
+var list_of_a = []
+checkList.classList.add('visible')
+
+document.getElementById("mouse-pad-mode").click();
+document.getElementById("design-mode").click();
+
+document.querySelector('#listOfA').addEventListener('change', updateAllPassCoeff)
+document.querySelector('#new-all-pass-coef').addEventListener('click', addNewA)
+
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    return response.json()
+}
+
+//design and draw frequency responce mag
+async function updateFilterDesign(data) {
+    data.gain = 1
+    let { w, angels, magnitude } = await postData(`${API}/getFilter`, data)
+    plotlyMultiLinePlot(filterDesignMagnitude, [
+        { x: w, y: magnitude, line: { color: '#febc2c' } },
+    ])
+}
+
+checkList.getElementsByClassName('anchor')[0].onclick = function () {
+    if (checkList.classList.contains('visible'))
+        checkList.classList.remove('visible');
+    else
+        checkList.classList.add('visible');
+}
+// add all pass filter coef in drop list 
+function addNewA() {
+    const old_li = document.querySelector('#listOfA').children
+
+    var old_list = []
+    for (let i =0; i<old_li.length;i++){
+    
+        old_list.push(old_li[i].children[0].checked)
+    }
+    var newA = document.getElementById('new-value').value
+   
+    if(newA > 1 || newA < -1){
+        alert(`invalid ${newA} as Filter Coefficient`)
+        return
+    }
+    if (!newA){
+        alert(`please enter a value for a`)
+        return
+    }
+    document.getElementById(
+        'listOfA'
+    ).innerHTML += `<li><input class = "target1" type="checkbox" data-avalue="${newA}" value="${newA}" checked/>${newA}</li>`
+
+    const new_li = document.querySelector('#listOfA').children
+
+    for (let i =0; i<old_li.length;i++){
+
+        new_li[i].children[0].checked = old_list[i]
+    }
+}
+// post request zeros and poles and angles all pass coef
+async function updateFilterPhase(allPassCoeff){
+    const { zeros, poles } = filter_plane.getZerosPoles(radius)
+    const { angels: allPassAngels } = await postData(
+        'http://127.0.0.1:8080/getAllPassFilter',
+        {
+            a: allPassCoeff,
+        }
+    )
+    const { w, angels: finalFilterPhase } = await postData(
+        'http://127.0.0.1:8080/getFinalFilter',
+        {
+            zeros,
+            poles,
+            a: allPassCoeff,
+        }
+    )
+    updateFilterPlotting(w, allPassAngels, finalFilterPhase)
+}
+//update draw plots 
+function updateFilterPlotting(w, allPassAngels, finalFilterPhase){
+    plotlyMultiLinePlot(allPassPhase, [{x: w, y: allPassAngels}])
+    plotlyMultiLinePlot(finalPhase, [{x: w, y: finalFilterPhase}])
+    plotlyMultiLinePlot(filterDesignPhase, [{x: w, y: finalFilterPhase}])
+}
+//function to set plotly 
+function plotlyMultiLinePlot(container, data){
+    Plotly.newPlot(
+        container,
+        data,
+        {
+            margin: { l: 30, r: 0, b: 30, t: 0 },
+            xaxis: {
+                autorange: true,
+                tickfont: { color: '#cccccc' },
+            },
+            yaxis: {
+                autorange: true,
+                tickfont: { color: '#cccccc' },
+            },
+            plot_bgcolor: '#111111',
+            paper_bgcolor: '#111111',
+        },
+        { staticPlot: true }
+    )
+}
+
+function arrayRemove(arr, value) {
+    return arr.filter(function (ele) {
+        return ele != value
+    })
+}
+// update all pass coef
+function updateAllPassCoeff(){
+    let allPassCoeff = []
+    document.querySelectorAll('.target1').forEach(item => {
+        let aValue = parseFloat(item.dataset.avalue)
+        let checked = item.checked
+        if (checked) allPassCoeff.push(aValue)
+    })
+    updateFilterPhase(allPassCoeff)
+}
+
+//change mode between zeros and poles 
+function changeMode(e){
+    unit_circle_mode = modesMap[e.target.id]
+    for(btn of modes_btns){
+        btn.style.color = (btn !== e.target) ? "#fff" : "#febc2c";
+    }
+}
+
+//change tabs between mouse pad and csv , between design filter and all pass filter
+function openMode(evt, selectedDiv,content,links ) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName(content);
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName(links);
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(selectedDiv).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+
+  //################################################################################################
+// gallery all pass filter 
+const gallery_div = document.getElementById('gallery').children
+var gallery = []
+var gallery_status = []
+for (let i=0; i<gallery.length; i++){
+    gallery.push(gallery_div[i].name)
+    gallery_status.push(false)
+}
+
+
+$("#gallery > img").click(function () {
+    addA(this.name)
+});
+
+
+// add filters from images
+function addA(newA) {
+    const old_li = document.querySelector('#listOfA').children
+
+    var old_list = []
+    for (let i =0; i<old_li.length;i++){
+ 
+        old_list.push(old_li[i].children[0].checked)
+    }
+    if(newA > 1 || newA < -1){
+        alert(`invalid ${newA} as Filter Coefficient`)
+        return
+    }
+    document.getElementById(
+        'listOfA'
+    ).innerHTML += `<li><input class = "target1" type="checkbox" data-avalue="${newA}" value="${newA}" checked>${newA}</li>`
+    
+    const new_li = document.querySelector('#listOfA').children
+
+    for (let i =0; i<old_li.length;i++){
+
+        new_li[i].children[0].checked = old_list[i]
+    }
+
+}
+
+// return the coef of all pass filters
+function get_a_list(){
+    
+    var l = document.querySelector('#listOfA').children
+    var list_of_a = []
+    for (let i = 0; i<l.length;i++){
+      
+        if(l[i].children[0].checked){
+        list_of_a.push(parseFloat(l[i].children[0].value))}
+
+    }
+    return list_of_a
+}
+
+// all pass filters clear button
+all_pass_clear_btn.addEventListener('click', function(e){
+    console.log("c")
+    document.getElementById('list1').innerHTML = '<span class="anchor btn">All Pass Filter Coefficients</span><ul id="listOfA" class="items"></ul>'
+})
+
---
+++ client/realtimeplotting.js
@@ -0,0 +1,206 @@
+live_input_container = document.getElementById("live-input")
+live_output_container = document.getElementById("live-output")
+const submit_btn = document.getElementById('csv-submitter')
+var input_arr_y= new Array(200).fill(0);
+var input_arr_x= Array.from(Array(200).keys())
+var output_arr_y=new Array(200).fill(0);
+const csvFile = document.getElementById('csvFile')
+var signal_x_csv, signal_y_csv
+var pause_location = 0
+var pause_bool = false
+var pause_button = document.getElementById("Pause")
+var restart_button = document.getElementById("Restart")
+var stop_button = document.getElementById("Stop")
+var live_input_x = Array.from(Array(200).keys())
+var live_input_y = new Array(200).fill(0); //X
+var live_output_y = new Array(200).fill(0); //Y
+
+
+
+// pause button 
+pause_button.addEventListener("click", function (evt){
+    pause_bool = !pause_bool
+    if (pause_bool == false){
+        drawCsv()
+        pause_button.innerHTML="Pause"
+    }else pause_button.innerHTML="Continue"
+})
+// restart button
+restart_button.addEventListener("click", function (evt){
+    pause_bool = false
+    pause_location = 0
+    live_input_x = Array.from(Array(200).keys())
+    live_input_y = new Array(200).fill(0)
+    live_output_y = new Array(200).fill(0);
+    drawCsv()
+    
+})
+stop_button.addEventListener("click", function (){
+    pause_bool = true
+    pause_location = -1
+    live_input_x = Array.from(Array(200).keys())
+    live_input_y = new Array(200).fill(0)
+    live_output_y = new Array(200).fill(0);
+
+    input_arr_y= new Array(200).fill(0);
+    input_arr_x= Array.from(Array(200).keys())
+    output_arr_y=new Array(200).fill(0);
+
+})
+// define plotly
+function plotlyMultiLinePlot(container, data){
+    return Plotly.newPlot(
+        container,
+        data,
+        {
+            margin: { l: 30, r: 0, b: 30, t: 0 },
+            xaxis: {
+                autorange: true,
+                tickfont: { color: '#cccccc' },
+            },
+            yaxis: {
+                autorange: true,
+                tickfont: { color: '#cccccc' },
+            },
+            plot_bgcolor: '#111111',
+            paper_bgcolor: '#111111',
+        },
+        { staticPlot: true }
+    )
+}
+
+// mouse pad
+var canvas = document.getElementById("mousepad");
+canvas.addEventListener("mousemove", async function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    input_arr_y.push((-mousePos.y+canvas.height)/2)
+
+    if(input_arr_y.length > 200){
+        input_arr_y.shift()
+ 
+    }
+    // draw input and output of mouse pad
+    plotlyMultiLinePlot(live_input_container, [{x: input_arr_x, y: input_arr_y}])
+    var {zeros, poles} = filter_plane.getZerosPoles(radius)
+    //add all pass
+    var all_pass_a = get_a_list()
+    // console.log(all_pass_a)
+    for (let i =0;i<all_pass_a.length;i++){
+        zeros.push([1/all_pass_a[i],0])
+        poles.push([all_pass_a[i],0])
+    }
+    const [a, b] = await get_differenceEquationCoefficients(zeros, poles)
+    a[0] = 0
+    var ya = output_arr_y.slice(-a.length);
+    ya = ya.reverse()
+    var xb = input_arr_y.slice(-b.length);
+    xb = xb.reverse()
+    //summision
+    var new_value = 0
+    for (let i=0; i<a.length; i++){
+        new_value += (ya[i])*(-a[i])+(xb[i]*b[i])
+    }
+    output_arr_y.push(new_value)
+    output_arr_y.shift()
+
+
+    plotlyMultiLinePlot(live_output_container, [{x: input_arr_x, y: output_arr_y}])
+});
+
+//Get Mouse Position
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+// get length of a coef of y and b coef of x
+function equateLength(a, b){
+    max_length = Math.max(a.length, b.length)
+    for(let i = 0; i < max_length; i++){
+        a[i] = i < a.length ? a[i] : 0
+        b[i] = i < b.length ? b[i] : 0
+    }
+    return [a, b]
+}
+
+async function get_differenceEquationCoefficients(zeros, poles) {
+    const {a, b} = await postData(`${API}/differenceEquationCoefficients`, {
+        zeros: zeros,
+        poles: poles,
+    });
+    equateLength(a, b);
+    return [a, b]
+}
+
+
+
+/////////////////
+
+// to convert matrix of data frame to column x,y
+function getCol(matrix, col) {
+    var column = []
+    for (var i = 0; i < matrix.length; i++) {
+        column.push(matrix[i][col])
+    }
+    return column
+}
+// initiate csv files
+csvFile.addEventListener('change', () => {
+    pause_bool = false
+    readData()
+})
+
+async function readData() {
+    live_input_x = Array.from(Array(200).keys())
+    live_input_y = new Array(200).fill(0); //X
+    live_output_y = new Array(200).fill(0); //Y
+    const input = csvFile.files[0]
+    const df = await dfd.read_csv(input)
+    signal_x_csv = getCol(df.values, 0)
+
+    signal_y_csv = getCol(df.values, 1)
+    drawCsv()
+
+}
+
+async function drawCsv(){
+    for (let i = pause_location; i < signal_x_csv.length; i++){
+        var {zeros, poles} = filter_plane.getZerosPoles(radius)
+        var all_pass_a = get_a_list()
+        for (let i =0;i<all_pass_a.length;i++){
+            zeros.push([1/all_pass_a[i],0])
+            poles.push([all_pass_a[i],0])
+        }
+        const [a, b] = await get_differenceEquationCoefficients(zeros, poles) 
+        a[0] = 0
+     
+        setTimeout(() => {
+            live_input_y.push(signal_y_csv[i])
+            live_input_y.shift()
+            plotlyMultiLinePlot(live_input_container, [{x: live_input_x, y: live_input_y}])
+            
+            var ya = live_output_y.slice(-a.length);
+            ya = ya.reverse()
+            var xb = live_input_y.slice(-b.length);
+            xb = xb.reverse()
+            var new_value = 0
+            for (let i=0; i<a.length; i++){ 
+            new_value += (ya[i])*(-a[i])+(xb[i]*b[i])
+            }
+            live_output_y.push(new_value)
+            live_output_y.shift()
+            plotlyMultiLinePlot(live_output_container, [{x: live_input_x, y: live_output_y}])
+           
+            pause_location = pause_location+1
+            
+
+        }, 1);
+        if (pause_bool == true){
+            break
+        }
+        
+    }
+}
+
